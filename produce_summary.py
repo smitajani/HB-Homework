e3aaebef4f94dd070ba0719d0e511b836eb80aa8
Smita Jani <smita.jani@gmail.com>
2020-05-03 19:22:17 +0000

    Code cleanup and optimization complete

--- produce_summary.py
+++ produce_summary.py
@@ -5,47 +5,51 @@
 # Iteration-1: Fix the report to print correct values - melon, count & amount, 
 # on each line
 
+# Iteration:2: Code cleanup and optimization
 
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+# Open the file object passed as the argument, loop through each row and do the
+# following:
+# - Strip spaces on the right
+# - Split the values separated by '|' into a list
+# - Print the values in a readable format on the report
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+def print_report(the_file, day_num):
+    print(f"Day {day_num}: {the_file.name}")
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    print(f"---- (EOF) End of file #{day_num}----\n\n")
+    the_file.close()
 
 
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+# Assign file names to a list and loop through. This replaces the code block 
+# below that called the function for each individual file 
+# ----------------------------------------------------------
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+list_of_files = []
+list_of_files = ["um-deliveries-20140519.txt", "um-deliveries-20140520.txt", "um-deliveries-20140521.txt"]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+for index, current_file in enumerate(list_of_files):
+    current_file = open(current_file)
+    print_report(current_file, index)
+
+
+# current_file = open("um-deliveries-20140519.txt")
+# print_report(current_file, 1)
+
+
+# current_file = open("um-deliveries-20140520.txt")
+# print_report(current_file, 2)
+
+
+# current_file = open("um-deliveries-20140521.txt")
+# print_report(current_file, 3)
