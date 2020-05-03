d5d9746e2b267e16914caef73cf202224f050629
Smita Jani <smita.jani@gmail.com>
2020-05-03 14:47:12 +0000

    Changed to print file for Monday sales

--- process.py
+++ process.py
@@ -7,15 +7,16 @@
 
 log_file = open("um-server-01.txt")     # Open file 'um-server-01.txt'
 
-### Question: What is log_file - file 'handle' for the open file?
-
 def sales_reports(log_file):            # Accept file handle(?) as argument
     for line in log_file:               # Loop through each line on the file 
         line = line.rstrip()            # Strip spaces to the right 
         day = line[0:3]                 # Get the day by extracting the first 
                                         #   three characters of the string
-        if day == "Tue":                # If it equals Tue, then print the line 
+        # Original Code - If it equals Tue, then print the line   
+        # Updated to instead check for 'Mon' and print the line                                     
+        #if day == "Tue":               
+        if day == "Mon":
             print(line)
 
-
+# Call the function 'sales_reports' passing the file objecrt as the argument
 sales_reports(log_file)
